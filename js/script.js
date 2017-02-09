8556978f0c673a084bc07ae07f77a68898895444
Ashley <pinedaa@spu.edu>
2017-02-09 00:41:47 -0800

    json loop added

--- js/script.js
+++ js/script.js
@@ -48,3 +48,10 @@ var products = [
     "imageTitle": "twill.jpg"
   }
 ]
+//ADD JSON LOOP
+
+for (var i = 0; i < products.length; i++) {
+  console.log(products[i].name);
+  console.log(products[i].price);
+  console.log(products)[i].description);
+}
