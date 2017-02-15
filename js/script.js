a1295e12675fa949e7b964fdddd05835aa288de6
Ashley <pinedaa@spu.edu>
2017-02-14 18:50:05 -0800

    change typo

--- js/script.js
+++ js/script.js
@@ -53,5 +53,5 @@ var products = [
 for (var i = 0; i < products.length; i++) {
   console.log(products[i].name);
   console.log(products[i].price);
-  console.log(products)[i].description);
+  console.log(products[i].description);
 }
